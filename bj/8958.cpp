bd91ed5b0c4de0ff80d2b9f9e2d12495a88c1aff
koll0070 <59383782+koll0070@users.noreply.github.com>
2021-02-24 11:28:24 +0900

    Update 8958.cpp

--- bj/8958.cpp
+++ bj/8958.cpp
@@ -4,19 +4,14 @@ int main()
 {
 	int n;
 	scanf("%d", &n);
-	for(int i=0; i<n; i++)
-    {
+	for(int i=0; i<n; i++) {
         char s[80];
         int cnt = 0, score = 0;
         scanf("%s", s);
-        for(int j=0; s[j]; j++)
-        {
-            if(s[j] == 'X')
-            {
+        for(int j=0; s[j]; j++) {
+            if(s[j] == 'X'){
                 cnt = 0;
-            }
-            else
-            {
+            } else {
                 cnt++;
                 score += cnt;
             }
